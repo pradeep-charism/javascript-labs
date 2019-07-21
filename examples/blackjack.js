ff8a88613c1e2e41fccbd1bf62b651bff258a9b6
pradeep-charism <pradeep.charism@gmail.com>
2019-07-22 00:04:28 +0800

    blackjack js

--- examples/blackjack.js
+++ examples/blackjack.js
@@ -6,14 +6,27 @@ let values = ['Ace', 'King', 'Queen', 'Jack',
               'Eight','Nine','Ten'
   ];
 
-  let deck = [];
+  let deck = createDeck();
+
+
+let playerCards = [ getNextCard(), getNextCard()];
+console.log("Welcome to blackjack game.");
+console.log(" "+   playerCards[0]);
+console.log(" "+   playerCards[1]);
+
+function getNextCard(){
+  return deck.shift();
+}
+
+function createDeck(){
+let deck = [];
 
   for(let i=0; i< suites.length; i++){
     for(let j=0; j < values.length; j++){
-      console.log(values[j] + ' of '+ suites[i]);
+      //console.log(values[j] + ' of '+ suites[i]);
       deck.push(values[j] + ' of '+ suites[i]);
     }
   }
-
-console.log(deck);
-
+  console.log(deck);
+  return deck;
+}
