6b1c85af54728d8b332272754aa7ca51f2a1558b
pradeep-charism <pradeep.charism@gmail.com>
2019-07-22 00:58:09 +0800

    html and js working together

--- examples/blackjack.js
+++ examples/blackjack.js
@@ -1,35 +1,45 @@
 
+// DOM
+let newGameButton = document.getElementById('new-game-button'),
+  textArea = document.getElementById('text-area'),
+  hitButton = document.getElementById('hit-button'),
+  stayButton = document.getElementById('stay-button');
 
-let textArea=document.getElementById('text-area');
-textArea.innerText='Hello world!';
+//textArea.innerText='Hello world!';
+textArea.style.display = "none";
 
-let okButton=document.getElementById('ok-button');
-okButton.innerText='Ok!';
-okButton.addEventListener('click', function(){
-    textArea.innerText="Button Clicked";
+hitButton.style.display = "none";
+stayButton.style.display = "none";
+
+newGameButton.addEventListener('click', function () {
+  textArea.style.display = "block";
+  textArea.innerText = "Game Started";
+  newGameButton.style.display = 'none';
+  hitButton.style.display = 'inline';
+  stayButton.style.display = 'inline';
 });
 
 let suites = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
 
 let values = ['Ace', 'King', 'Queen', 'Jack',
-              'One','Two','Three','Four','Five','Six','Seven',
-              'Eight','Nine','Ten'
-  ];
+  'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+  'Eight', 'Nine', 'Ten'
+];
 
-  let deck = createDeck();
+let deck = createDeck();
 
 
-let playerCards = [ getCardString(getNextCard()),
-                    getCardString(getNextCard())];
+let playerCards = [getCardString(getNextCard()),
+getCardString(getNextCard())];
 console.log("Welcome to blackjack game.");
-console.log(" "+   playerCards[0]);
-console.log(" "+   playerCards[1]);
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
 
-function getNextCard(){
+function getNextCard() {
   return deck.shift();
 }
 
-function getCardString(card){
+function getCardString(card) {
   return card.value + ' of ' + card.suit;
 }
 
@@ -39,14 +49,14 @@ let card = {
   value: 'Ace'
 };
 
-function createDeck(){
-let deck = [];
+function createDeck() {
+  let deck = [];
 
-  for(let i=0; i< suites.length; i++){
-    for(let j=0; j < values.length; j++){
+  for (let i = 0; i < suites.length; i++) {
+    for (let j = 0; j < values.length; j++) {
       //console.log(values[j] + ' of '+ suites[i]);
       //deck.push(values[j] + ' of '+ suites[i]);
-      let card = { suit: suites[i], value: values[j]};
+      let card = { suit: suites[i], value: values[j] };
       deck.push(card);
     }
   }
